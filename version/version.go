d19879c86e9820a8be7a1569b112b33a825a750d
Anthony Kim <jikime@gmail.com>
2026-01-24 15:22:49 +0900

    chore: bump version to 0.0.2

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 // buildVersion is injected at build time via -ldflags "-X 'jikime-adk/version.buildVersion=<value>'".
 var buildVersion string
 
-const fallbackVersion = "2.0.0"
+const fallbackVersion = "0.0.2"
 
 func String() string {
 	if buildVersion != "" {
