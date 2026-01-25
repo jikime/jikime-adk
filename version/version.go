c0c90707a5e05894a3cddf289cf257f832693390
Anthony Kim <jikime@gmail.com>
2026-01-25 14:08:52 +0900

    feat:advanced commands, agents, skills, orchestration, llm router

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 // buildVersion is injected at build time via -ldflags "-X 'jikime-adk/version.buildVersion=<value>'".
 var buildVersion string
 
-const fallbackVersion = "0.0.1"
+const fallbackVersion = "0.1.1"
 
 func String() string {
 	if buildVersion != "" {
