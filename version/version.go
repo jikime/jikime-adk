1e848c441f01f4e955188f06f3784e76bfbf0a17
Anthony Kim <jikime@gmail.com>
2026-02-24 10:58:01 +0900

    fix:removed unnessary files and code

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 // buildVersion is injected at build time via -ldflags "-X 'jikime-adk/version.buildVersion=<value>'".
 var buildVersion string
 
-const fallbackVersion = "0.8.2"
+const fallbackVersion = "0.8.3"
 
 func String() string {
 	if buildVersion != "" {
